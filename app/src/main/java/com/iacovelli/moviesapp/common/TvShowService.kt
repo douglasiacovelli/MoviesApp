a11fb3dbbec54eab79fb7619ea8456ae97d2d80c
Douglas Iacovelli <douglas.iacovelli@gmail.com>
2018-04-04 02:10:47 -0300

    adds details interface

--- app/src/main/java/com/iacovelli/moviesapp/common/TvShowService.kt
+++ app/src/main/java/com/iacovelli/moviesapp/common/TvShowService.kt
@@ -3,10 +3,14 @@ package com.iacovelli.moviesapp.common
 import com.iacovelli.moviesapp.models.TvShowResponse
 import io.reactivex.Single
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface TvShowService {
 
     @GET("tv/popular")
     fun getTvShows(@Query("page") page: Int): Single<TvShowResponse>
+
+    @GET("tv/{id}?append_to_response=similar")
+    fun getTvShow(@Path("id") tvShowId: Int)
 }
